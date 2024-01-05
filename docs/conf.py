ce2f6287dfac67bfe3e3c1597a2b6de6da07b755
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-01-05 22:38:41 +0100

    update docs paths/install bib_dedupe

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../bib_dedupe'))
 
 # Configuration file for the Sphinx documentation builder.
 #
