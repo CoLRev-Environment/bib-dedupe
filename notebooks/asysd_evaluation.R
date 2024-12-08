7922e1f19ee6c15784fc2e4e7194e9285cf7dfe5
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-12-08 16:52:29 +0100

    fix: set user_input=1 for ASySD

--- notebooks/asysd_evaluation.R
+++ notebooks/asysd_evaluation.R
@@ -7,15 +7,29 @@ library(dplyr)
 filepath <- commandArgs(trailingOnly = TRUE)[1]
 # filepath <- "records_pre_merged.csv"
 
+# Print the filepath
+cat("Filepath provided:", filepath, "\n")
+
+# Check if the file exists
+if (file.exists(filepath)) {
+  cat("The file exists.\n")
+} else {
+  stop("The file does not exist. Please check the filepath.\n")
+}
+
 # load citations
 citation_data <- load_search(filepath, method="csv") # endnote
+
+# Print column names of citation_data
+cat("Column names in citation_data:\n")
+print(colnames(citation_data))
 
 # Set ID
 citation_data$record_id <- citation_data$ID
 citation_data <- citation_data[, -which(names(citation_data) == "ID")]
 
 # deduplicate
-dedup_citations <- dedup_citations(citation_data)
+dedup_citations <- dedup_citations(citation_data, user_input=1)
 
 # get unique citation dataframe
 unique_citations <- dedup_citations$unique
